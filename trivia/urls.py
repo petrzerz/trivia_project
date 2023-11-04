03fb9018f046bd0e60a4ca3129f7cff9e3ebda91
pzerzis <pzerzis@wings-ict-solutions.eu>
2023-11-04 19:38:32 +0200

    first html

--- trivia/urls.py
+++ trivia/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from trivia_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.question_list, name='question_list'),
+    path('<int:question_id>/', views.question_detail, name='question_detail'),
+
 ]
--- trivia_app/views.py
+++ trivia_app/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Question, Category, Answer
 
-# Create your views here.
+from django.shortcuts import render
+from .models import Question, Category
+
+
+def question_list(request):
+    categories = Category.objects.all()
+    selected_category = request.GET.get('category')
+    if selected_category:
+        questions = Question.objects.filter(category__name=selected_category)
+    else:
+        questions = Question.objects.all()
+    return render(request, 'question_list.html', {'questions': questions, 'categories': categories})
+
+
+def question_detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'question_detail.html', {'question': question})
