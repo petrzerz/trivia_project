621d93e9c70ef6918abdb2065b93e03279b9caa2
pzerzis <pzerzis@wings-ict-solutions.eu>
2023-11-06 17:03:09 +0200

    format

--- trivia_app/tests/test_question_list.py
+++ trivia_app/tests/test_question_list.py
@@ -13,6 +13,7 @@ def test_question_list_url():
     path = reverse('question_list')
     assert resolve(path).view_name == 'question_list'
 
+
 # Test whether the correct template is used for the question list view
 @pytest.mark.django_db
 def test_question_list_template():
@@ -21,6 +22,7 @@ def test_question_list_template():
     response = test_client.get(url)
     assert response.status_code == 200
     assertTemplateUsed(response, 'question_list.html')
+
 
 # Test the question list view by creating test data using factories
 @pytest.mark.django_db
@@ -34,6 +36,7 @@ def test_question_list_view():
     request = RequestFactory().get("/")
     response = question_list(request)
     assert response.status_code == 200
+
 
 # Test the question list view with category filtering
 @pytest.mark.django_db
@@ -55,6 +58,7 @@ def test_question_list_with_category_filter():
     assert str(question1.text) in content
     assert str(question2.text) not in content
 
+
 # Test the question list view with difficulty filtering
 @pytest.mark.django_db
 def test_question_list_with_difficulty_filter():
@@ -75,6 +79,7 @@ def test_question_list_with_difficulty_filter():
     assert str(question1.text) in content
     assert str(question2.text) not in content
 
+
 # Test the validity of the form with valid data
 @pytest.mark.django_db
 def test_question_list_form_valid():
@@ -87,6 +92,7 @@ def test_question_list_form_valid():
     }
     form = FilterForm(data=form_data)
     assert form.is_valid()
+
 
 # Test the invalidity of the form with invalid data
 @pytest.mark.django_db
